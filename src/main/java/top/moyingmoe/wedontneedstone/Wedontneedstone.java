09e705c0209a0d4a0cf3188eedb5e4e4f1127a20
Moying-moe <741744469@qq.com>
2022-09-06 20:31:54 +0800

    initial

---
+++ src/main/java/top/moyingmoe/wedontneedstone/Wedontneedstone.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2022 moying All Rights Reserved.
+ *
+ * Distributed under MIT license.
+ * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+ */
+
+package top.moyingmoe.wedontneedstone;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Wedontneedstone implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
