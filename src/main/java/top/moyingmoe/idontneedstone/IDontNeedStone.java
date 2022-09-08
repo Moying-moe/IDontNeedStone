6a48af759efa94162ec5dd0a85c406008aba0894
Moying-moe <741744469@qq.com>
2022-09-08 19:58:26 +0800

    存在一个问题，在服务器上无法运行。需要自己做个数据包

--- src/main/java/top/moyingmoe/idontneedstone/IDontNeedStone.java
+++ src/main/java/top/moyingmoe/idontneedstone/IDontNeedStone.java
@@ -11,18 +11,10 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.MutableText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import top.moyingmoe.idontneedstone.api.event.ItemEntityPickupCallback;
-import top.moyingmoe.idontneedstone.config.Config;
-import top.moyingmoe.idontneedstone.config.ConfigManager;
 import top.moyingmoe.idontneedstone.handler.FabricItemPickupHandler;
-import top.moyingmoe.idontneedstone.hotkey.HotkeyHandler;
-import top.moyingmoe.idontneedstone.hotkey.HotkeyManager;
 
 public class IDontNeedStone implements ModInitializer {
     public static final String MOD_ID = "idontneedstone";
@@ -33,39 +25,5 @@ public class IDontNeedStone implements ModInitializer {
     public void onInitialize() {
         final FabricItemPickupHandler itemPickupHandler = new FabricItemPickupHandler();
         ItemEntityPickupCallback.EVENT.register(itemPickupHandler::onEntityItemPickup);
-
-        Config config = ConfigManager.initializeConfig();
-
-        HotkeyManager.initial();
-        HotkeyManager.addHotkey("filter", new HotkeyHandler(config.getFilterHotkey()) {
-            @Override
-            protected void onPress(MinecraftClient client) {
-                Config config = ConfigManager.getConfig();
-                boolean isOn = !config.getIsFilterOn();
-                config.setIsFilterOn(isOn);
-
-                Text modName = Text.translatable("idontneedstone.name.bracket", Text.translatable("idontneedstone.name")).formatted(Formatting.GOLD);
-                Text titleText = Text.translatable("idontneedstone.screen.title.filter").formatted(Formatting.WHITE);
-
-                Text titleText2;
-                if (isOn) {
-                    titleText2 = Text.translatable("idontneedstone.screen.title.filter.on").formatted(Formatting.GREEN).formatted(Formatting.BOLD);
-                } else {
-                    titleText2 = Text.translatable("idontneedstone.screen.title.filter.off").formatted(Formatting.RED).formatted(Formatting.BOLD);
-                }
-                MutableText result = modName.copy();
-                result.append(titleText);
-                result.append(titleText2);
-                client.inGameHud.setOverlayMessage(result, true);
-            }
-            @Override
-            protected void onHold(MinecraftClient client) {
-
-            }
-            @Override
-            protected void onRelease(MinecraftClient client) {
-
-            }
-        });
     }
 }
--- src/main/java/top/moyingmoe/idontneedstone/client/IDontNeedStoneClient.java
+++ src/main/java/top/moyingmoe/idontneedstone/client/IDontNeedStoneClient.java
@@ -10,11 +10,51 @@ package top.moyingmoe.idontneedstone.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import top.moyingmoe.idontneedstone.config.Config;
+import top.moyingmoe.idontneedstone.config.ConfigManager;
+import top.moyingmoe.idontneedstone.hotkey.HotkeyHandler;
+import top.moyingmoe.idontneedstone.hotkey.HotkeyManager;
 
 @Environment(EnvType.CLIENT)
 public class IDontNeedStoneClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        Config config = ConfigManager.initializeConfig();
 
+        HotkeyManager.initial();
+        HotkeyManager.addHotkey("filter", new HotkeyHandler(config.getFilterHotkey()) {
+            @Override
+            protected void onPress(MinecraftClient client) {
+                Config config = ConfigManager.getConfig();
+                boolean isOn = !config.getIsFilterOn();
+                config.setIsFilterOn(isOn);
+
+                Text modName = Text.translatable("idontneedstone.name.bracket", Text.translatable("idontneedstone.name")).formatted(Formatting.GOLD);
+                Text titleText = Text.translatable("idontneedstone.screen.title.filter").formatted(Formatting.WHITE);
+
+                Text titleText2;
+                if (isOn) {
+                    titleText2 = Text.translatable("idontneedstone.screen.title.filter.on").formatted(Formatting.GREEN).formatted(Formatting.BOLD);
+                } else {
+                    titleText2 = Text.translatable("idontneedstone.screen.title.filter.off").formatted(Formatting.RED).formatted(Formatting.BOLD);
+                }
+                MutableText result = modName.copy();
+                result.append(titleText);
+                result.append(titleText2);
+                client.inGameHud.setOverlayMessage(result, true);
+            }
+            @Override
+            protected void onHold(MinecraftClient client) {
+
+            }
+            @Override
+            protected void onRelease(MinecraftClient client) {
+
+            }
+        });
     }
 }
